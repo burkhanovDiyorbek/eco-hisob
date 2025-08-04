8d2d35cc069423d1a613134b5fc1f398882f8683
Shohjahon <pazvat56@gmail.com>
2025-08-04 15:45:44 +0500

    feat: add url_launcher dependency and implement link navigation in home screen

--- lib/pages/home/view/home_screen.dart
+++ lib/pages/home/view/home_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home")));
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            launchUrl(Uri.parse("https://t.me/ergashevTech"));
+          },
+          child: const Text("Go to Telegram"),
+        ),
+      ),
+    );
   }
 }
-  
